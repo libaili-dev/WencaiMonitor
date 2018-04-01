2038057c07c292409ebf3c7a1710b91ed477426a
linfili <liboli.1988@hotmail.com>
2018-04-01 22:08:22 +0800

    Add project files.

---
+++ WencaiMonitor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WencaiMonitor
+{
+    public class Class1
+    {
+    }
+}
